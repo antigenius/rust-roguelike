d2170b1b4c1df9682bef26fb2e6707228ede6991
Matt Roberts <matt.roberts@discogsinc.com>
2025-03-07 12:37:52 -0800

    Organize code and randomize map
    Also adds various control schemes.
    
    Completes Chapter 4.
    https://bfnightly.bracketproductions.com/rustbook/chapter_4.html

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::cmp::{max, min};
+mod components;
+mod map;
+mod player;
+mod rect;
 
-use rltk::{GameState, Rltk, Tile, VirtualKeyCode, RGB};
+use rltk::{GameState, RGB, Rltk};
 use specs::prelude::*;
-use specs_derive::Component;
+
+use components::*;
+use map::*;
+use player::*;
+use rect::*;
 
 struct State {
-    ecs: World,
+    pub ecs: World,
 }
 
 impl State {
@@ -33,111 +40,6 @@ impl GameState for State {
     }
 }
 
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    bg: RGB,
-    fg: RGB,
-    glyph: rltk::FontCharType,
-}
-
-#[derive(Component, Debug)]
-struct Player {}
-
-#[derive(Clone, Copy, PartialEq)]
-enum TileType {
-    Floor,
-    Wall,
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gamestate: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {}
-        Some(key) => match key {
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gamestate.ecs),
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gamestate.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gamestate.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gamestate.ecs),
-            _ => {}
-        },
-    }
-}
-
-fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * 80) + x as usize
-}
-
-fn new_map() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80*50];
-
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, 49)] = TileType::Wall;
-    }
-
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(79, y)] = TileType::Wall;
-    }
-
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-
-        if idx != xy_idx(40, 25) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
-}
-
-fn draw_map(map: &[TileType], ctx: &mut Rltk) {
-    let mut x = 0;
-    let mut y = 0;
-
-    for tile in map.iter() {
-        match tile {
-            TileType::Floor => {
-                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
-            },
-            TileType::Wall => {
-                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
-            },
-        }
-
-        x += 1;
-
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-
 fn main() -> rltk::BError {
     let context = rltk::RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
@@ -147,12 +49,19 @@ fn main() -> rltk::BError {
     gamestate.ecs.register::<Renderable>();
     gamestate.ecs.register::<Player>();
 
-    gamestate.ecs.insert(new_map());
+    let (rooms, map) = new_map_rooms_and_corridors();
+
+    gamestate.ecs.insert(map);
+
+    let (player_x, player_y) = rooms[0].center();
 
     gamestate
         .ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: player_x,
+            y: player_y
+        })
         .with(Renderable {
             bg: RGB::named(rltk::BLACK),
             fg: RGB::named(rltk::YELLOW),
